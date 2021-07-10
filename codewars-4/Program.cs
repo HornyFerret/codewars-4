39f42ee6b332e2338cc13c26c139484670aa5e75
HornyFerret <ombro.de.morto@gmail.com>
2021-07-10 15:48:34 +0300

    Добавьте файлы проекта.

---
+++ codewars-4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace codewars_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "1234";
+            int w = StringToNumber(str);
+            Console.WriteLine(w);
+        }
+        public static int StringToNumber(String str)
+        {
+            //TODO: Convert str into a number
+            int n = Int32.Parse(str);
+            return n;
+        }
+    }
+}
